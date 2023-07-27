25d685ca3836eff4a569defa981718eb4cd6f8f6
Orlando Paulo <113864263+Orlando-Paulo99@users.noreply.github.com>
2023-07-27 10:44:05 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let jogador="x"
+
+
+function jogar(celula){
+    if(celula.innerHTML==""){
+        celula.innerHTML=jogador
+
+        if(jogador=="x"){
+            jogador="o"
+        }else{
+            jogador="x"
+        }
+    }
+    
+}
+
+function reset(){
+    window.location.reload()
+}
